fb47222285bd1768d7a7bb99bd3c86142a1bf486
Leonid Yurchenko <noco.mi@gmail.com>
2026-04-17 14:49:34 +0300

    Rework the 3d smoother

--- smoother3d.py
+++ smoother3d.py
@@ -1,4 +1,7 @@
+import math
+
 from .point3d import Point3D
+
 
 class Smoother3D:
     def __init__(self, start_pos: Point3D, max_speed: float, acceleration: float):
@@ -8,46 +11,44 @@ class Smoother3D:
         self._target_pos = start_pos
         self._current_speed = Point3D()
 
-    def set_target(self, target_pos):
+    def set_target(self, target_pos: Point3D):
         self._target_pos = target_pos
 
-    def get(self):
+    def get(self) -> Point3D:
         return self._current_pos
 
-    def tick(self, dt):
+    def tick(self, dt: float):
         target_distance = self._current_pos.distance(self._target_pos)
+        
+        # Snap to target if we are incredibly close to prevent micro-jitter
+        if target_distance < 0.001:
+            self._current_pos = self._target_pos
+            self._current_speed = Point3D()
+            return
+
         target_direction = (self._target_pos - self._current_pos).normalized()
         abs_speed = self._current_speed.magnitude()
-        # abs_target_distance = abs(target_distance)
 
-        if self.near_zero(target_distance):
+        # Maximum speed we can go and still stop in time
+        # Derived from: v^2 = v0^2 + 2ad
+        max_allowed_speed = math.sqrt(2.0 * self._acceleration * target_distance)
+        
+        # Goal speed is the lowest of either the physical limit or safe braking speed
+        goal_speed = min(self._max_abs_speed, max_allowed_speed)
+
+        # Smoothly accelerate or decelerate towards the goal speed
+        if abs_speed < goal_speed:
+            abs_speed = min(abs_speed + self._acceleration * dt, goal_speed)
+        else:
+            # max() ensures no negative speeds
+            abs_speed = max(abs_speed - self._acceleration * dt, 0.0)
+
+        self._current_speed = target_direction * abs_speed
+        step_vector = self._current_speed * dt
+
+        # Overshoot protection: Don't step further than the target distance
+        if step_vector.magnitude() >= target_distance:
+            self._current_pos = self._target_pos
             self._current_speed = Point3D()
         else:
-            braking_distance = (abs_speed ** 2) / (2 * self._acceleration)
-
-            if target_distance > braking_distance:
-                if self.near_zero(abs_speed - self._max_abs_speed):
-                    pass  # Cruising at max speed, do nothing
-                elif abs_speed < self._max_abs_speed:
-                    abs_speed = self.apply_acceleration(abs_speed, self._acceleration, dt)
-                elif abs_speed > self._max_abs_speed:
-                    abs_speed = self.apply_acceleration(abs_speed, -self._acceleration, dt)
-            else:
-                required_braking_accel = (abs_speed ** 2) / (2 * target_distance)
-                abs_speed = self.apply_acceleration(abs_speed, -required_braking_accel, dt)
-
-            self._current_speed = target_direction * abs_speed
-
-        delta_distance = self._current_speed * dt
-        self._current_pos += delta_distance
-
-        if self.near_zero((self._current_pos - self._target_pos).magnitude()):
-            self._current_pos = self._target_pos
-
-    def apply_acceleration(self, speed, accel, dt):
-        delta_a = accel * dt
-        return min(speed + delta_a, self._max_abs_speed)
-
-    @staticmethod
-    def near_zero(value, threshold=0.01):
-        return abs(value) < threshold
+            self._current_pos += step_vector
